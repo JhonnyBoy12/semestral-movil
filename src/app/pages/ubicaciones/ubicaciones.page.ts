ea434dfdfe5b93a0ee5dc47acfa0d51470e4db0d
Diego Cruz <dashlol45@gmail.com>
2024-08-31 17:07:59 -0400

    Avanzes menu, diseño y ubicaciones

--- src/app/pages/ubicaciones/ubicaciones.page.ts
+++ src/app/pages/ubicaciones/ubicaciones.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ubicaciones',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UbicacionesPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  mensajeProvisorio(){
+    this.router.navigate(['/info-ubicaciones']);
   }
 
 }
